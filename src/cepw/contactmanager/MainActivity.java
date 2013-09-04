c0b706f7186621416685dd9936447036b3b8f89f
Bert Huang <dendeer82@gmail.com>
2013-09-04 16:15:22 +1200

    Worked on the name fields (include expand/collapse). Added ScrollView to the activity.

--- src/cepw/contactmanager/MainActivity.java
+++ src/cepw/contactmanager/MainActivity.java
@@ -2,7 +2,9 @@ package cepw.contactmanager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getActionBar().setDisplayShowTitleEnabled(false);
+		
 	}
 
 	@Override
@@ -19,5 +22,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_add_new:
+			gotoCreateNewContact();
+			return true;
+		
+		case R.id.action_exit:
+			finish();
+			
+		default:
+			return true;
+		}
+	}
 
+	
+	private void gotoCreateNewContact() {
+		Intent i = new Intent(getApplicationContext(),
+				NewContactActivity.class);
+		startActivity(i);
+	}
+	
+	
 }
--- src/cepw/contactmanager/NewContactActivity.java
+++ src/cepw/contactmanager/NewContactActivity.java
@@ -2,18 +2,45 @@ package cepw.contactmanager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.support.v4.app.NavUtils;
 
 public class NewContactActivity extends Activity {
 
+	private ImageButton expandName, collapseName;
+	private EditText fullName, namePrefix, givenName, middleName, lastName, nameSuffix;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_contact);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		expandName = (ImageButton) findViewById(R.id.button_name_expand);
+		collapseName = (ImageButton) findViewById(R.id.button_name_collapse);
+		fullName = (EditText) findViewById(R.id.textfield_name_full);
+		namePrefix = (EditText) findViewById(R.id.textfield_name_prefix);
+		givenName = (EditText) findViewById(R.id.textfield_name_given);
+		middleName = (EditText) findViewById(R.id.textfield_name_middle);
+		lastName = (EditText) findViewById(R.id.textfield_name_surname);
+		nameSuffix = (EditText) findViewById(R.id.textfield_name_suffix);
+		
+		collapseName.setVisibility(View.GONE);
+		namePrefix.setVisibility(View.GONE);
+		givenName.setVisibility(View.GONE);
+		middleName.setVisibility(View.GONE);
+		lastName.setVisibility(View.GONE);
+		nameSuffix.setVisibility(View.GONE);
+		
 	}
 
 	/**
@@ -22,6 +49,7 @@ public class NewContactActivity extends Activity {
 	private void setupActionBar() {
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setDisplayShowTitleEnabled(false);
 
 	}
 
@@ -45,8 +73,34 @@ public class NewContactActivity extends Activity {
 			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
+			
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void expandNameField(View v) {
+		expandName.setVisibility(View.GONE);
+		collapseName.setVisibility(View.VISIBLE);
+		
+		fullName.setVisibility(View.GONE);
+		namePrefix.setVisibility(View.VISIBLE);
+		nameSuffix.setVisibility(View.VISIBLE);
+		givenName.setVisibility(View.VISIBLE);
+		middleName.setVisibility(View.VISIBLE);
+		lastName.setVisibility(View.VISIBLE);
+	}
+	
+	public void collapseNameField(View v) {
+		expandName.setVisibility(View.VISIBLE);
+		collapseName.setVisibility(View.GONE);
+		
+		fullName.setVisibility(View.VISIBLE);
+		namePrefix.setVisibility(View.GONE);
+		nameSuffix.setVisibility(View.GONE);
+		givenName.setVisibility(View.GONE);
+		middleName.setVisibility(View.GONE);
+		lastName.setVisibility(View.GONE);
+	}
+
 
 }
